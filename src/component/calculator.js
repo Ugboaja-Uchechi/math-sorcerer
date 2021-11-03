235d57b883d635c65f2688f373e9ff6ecacf4eef
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-03 13:53:47 +0100

    Add event and logic

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,37 +1,53 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class MyCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
+  buttonClicked = (e) => {
+    const buttonName = e.target.textContent;
+    const newObj = calculate({ ...this.state }, buttonName);
+    this.setState(newObj);
+  };
+
   render() {
+    const { total, next, operation } = this.state;
+    let result = 0;
+    if (total) {
+      result = `${total} ${operation || ''} ${next || ''}`;
+    } else if (next) {
+      result = next;
+    }
     return (
       <main>
-        <div className="result">
-          0
-        </div>
+        <div className="result">{result}</div>
         <div className="symbols">
-          <button type="submit">AC</button>
-          <button type="submit">+/-</button>
-          <button type="submit">%</button>
-          <button type="submit" className="orange">&#247;</button>
-          <button type="submit">7</button>
-          <button type="submit">8</button>
-          <button type="submit">9</button>
-          <button type="submit" className="orange">x</button>
-          <button type="submit">4</button>
-          <button type="submit">5</button>
-          <button type="submit">6</button>
-          <button type="submit" className="orange">-</button>
-          <button type="submit">1</button>
-          <button type="submit">2</button>
-          <button type="submit">3</button>
-          <button type="submit" className="orange">+</button>
-          <button type="submit" className="extra">0</button>
-          <button type="submit">.</button>
-          <button type="submit" className="orange">=</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">AC</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">+/-</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">%</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="orange">&#247;</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">7</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">8</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">9</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="orange">x</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">4</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">5</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">6</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="orange">-</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">1</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">2</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">3</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="orange">+</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="extra">0</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button">.</button>
+          <button onClick={(e) => this.buttonClicked(e)} type="button" className="orange">=</button>
         </div>
       </main>
     );
